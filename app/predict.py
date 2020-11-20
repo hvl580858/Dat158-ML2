e770a59bb74ceb13babf1a6ba2716579668405ec
Per Helge <per.h92@gmail.com>
2020-11-20 15:29:16 +0100

    Added comments to all parts of the deployment part of the application

--- app/predict.py
+++ app/predict.py
@@ -5,11 +5,22 @@ import joblib
 model = joblib.load('model/model.joblib')
 pipeline = joblib.load("model/pipeline.joblib")
 
+"""
+Preprocess:
+    This is the "cleaning" stage of the prediction. Here we put the values from the form into our preset and calculate
+    budget_year_ratio which turned out to be a good predictor. 
+    We then convert the dict to a pandas DataFrame and run that through our pipeline.
+    Output of our pipeline is a numpy Array.
+    We also grab the title from the form and remove if from the data before we start to enter the data into the 
+    feature_values dict. 
+    
+"""
+
 
 def preprocess(data):
     data.update({'budget_year_ratio': 0})
 
-    name = data.pop('name', None)
+    title = data.pop('name', None)
 
     feature_values = {
         'belongs_to_collection': 1,
@@ -46,21 +57,31 @@ def preprocess(data):
     df = pd.DataFrame(feature_values, index=[0])
     df = pipeline.transform(df)
 
-    return df, name
+    return df, title
+
+
+"""
+Predict:
+    This does what the name implies it will predict the revenue of the movie based on the input.
+    We take the predicted revenue and increase it exponentially subtracting 1 to get it back up to a value that makes 
+    sense to us. The reason for this is that it got a better prediction downscaling the labels before fitting the model,
+    but that means the value it predicts will be downscaled as well.
+    This last step we probably could have done in postprocess but we mad our life easy with short time remaining.
+"""
 
 
 def predict(data):
     pred = np.expm1(model.predict(data.reshape(1, -1)))
-
     return pred
 
 
-def postprocess(pred):
-    try:
-        int(pred[0]) > 0
-    except ValueError:
-        pass
+"""
+Postprocess: 
+    Here we make the prediction into a dictionary and return it.
+"""
 
+
+def postprocess(pred):
     pred = str(pred[0])
     return_dict = {'pred': pred}
     return return_dict
